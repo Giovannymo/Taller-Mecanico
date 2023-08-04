fdd701284382355d714ce8f5dfd2aed4afea18dd
Giovanny Moreno <44243879+Giovannymo@users.noreply.github.com>
2023-08-04 17:12:50 -0500

    First Commit :)

---
+++ Class/MainMenu.cs
@@ -0,0 +1,11 @@
+namespace Taller_Mecanico.Class;
+
+    public class MainMenu
+    {
+        public void Menu(){
+            Console.WriteLine("********** TALLER MECANICO **********");
+            Console.WriteLine("");
+
+        }
+
+    }
