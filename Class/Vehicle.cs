35f07ddfa0c3cfa90b5a8eb7db4c7f8a0b915847
Giovanny Moreno <giovannysocas@gmail.com>
2023-08-05 18:27:47 -0500

    feat: Only created Class Vehicle

---
+++ Class/Vehicle.cs
@@ -0,0 +1,14 @@
+namespace Taller_Mecanico.Class;
+
+    public class Vehicle
+    {
+        public int id;
+        public int plate;
+        public string model;
+        public string colour;
+        public int km;
+
+        
+
+
+    }
