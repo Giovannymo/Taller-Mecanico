75585a19e0ad44af3530b2b2ffb05d94ca05a4ed
Giovanny Moreno <giovannysocas@gmail.com>
2023-08-06 18:23:23 -0500

    feat: Class Workshop

--- Class/Workshop.cs
+++ Class/Workshop.cs
@@ -19,6 +19,68 @@ namespace Taller_Mecanico.Class;
             this.employees = new List<Employee>();
         }
 
+        public int Id{
+            get{ return id; }
+            set{ id = value; }
+        }
+        public string Name{
+            get{ return name; }
+            set{ name = value; }
+        }
+        public string Owner{
+            get{ return owner; }
+            set{ owner = value; }
+        }
+        public List<Customer> Customers{
+            get{ return customers; }
+            set{ customers = value; }	
+        }
+        public List<Employee> Employees{
+            get{ return employees; }
+            set{ employees = value; }
+        }
 
-        
+
+/// <summary>
+/// Show all employees in Employess List
+/// 
+/// </summary>
+        public void ShowEmployees(){
+            foreach (Employee employee in employees){
+                Console.WriteLine($"Cedula: \t\t Nombre: \t\t Telefono \t\t Especialidades: ");
+                Console.Write(employee.Id+"\t");
+                Console.Write(employee.Name+"\t");
+                Console.Write(employee.NumberPhone+"\t");
+                foreach(string speciality in employee.Speaciality){
+                    Console.Write(speciality + ", ");
+                }
+               
+            }
+        }
+        /// <summary>
+        /// Show All List Customers 
+        /// </summary>
+        public void ShowCustomers(){
+            foreach (Customer customer in customers){
+                Console.WriteLine($"Cedula: \t\t Nombre: \t\t Telefono: \t\t Email: \t\t\t  Creado:");
+                Console.Write(customer.Id+"\t");
+                Console.Write(customer.Name+"\t");
+                Console.Write(customer.NumberPhone+"\t");
+                Console.Write(customer.email+"\t\n");
+                Console.Write(customer.CreatedAt.ToString());
+                Console.WriteLine("Vehiculos del cliente: ");
+                foreach(Vehicle vehicle in customer.vehicles){
+                    Console.WriteLine("Placa: \t\t Modelo: \t\t Marca: \t\t Color: \t\t Kilometraje:" );
+                    Console.Write(vehicle.Plate + ", ");
+                    Console.Write(vehicle.Model + ", ");
+                    Console.Write(vehicle.Brand + ", ");
+                    Console.Write(vehicle.Color + ", ");
+                    Console.Write(vehicle.Km + ". ");
+                }
+               
+            }
+        }
+
+
+
 }
