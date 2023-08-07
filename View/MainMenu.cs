d701c154bde780fe18f9364757ff492286023cf1
Giovanny Moreno <giovannysocas@gmail.com>
2023-08-06 20:58:29 -0500

    feat: Create new cliente menu

--- View/MainMenu.cs
+++ View/MainMenu.cs
@@ -3,9 +3,9 @@ namespace Taller_Mecanico.Class;
     public class MainMenu
     {
         public int Menu(){
-            int option =0;
+            int option =1;
+
             try{
-                Console.Clear();
                 Console.WriteLine("********** TALLER MECANICO **********");
                 Console.WriteLine("1. REGISTRAR CLIENTE. " + "\n"
                                 + "2. ELIMINAR CLIENTE."  + "\n"
@@ -16,12 +16,14 @@ namespace Taller_Mecanico.Class;
                                 + "7. ELIMINAR VEHICULO A CLIENTE EXISTENTE."  + "\n"
                                 + "8. GENERAR UNA ORDEN DE SERVICIO AL CLIENTE."  + "\n"
                                 + "9. AGREGAR UNA NUEVA ORDEN DE APROBACION ."  + "\n"
-                                + "10. ENTREGAR VEHICULO(S) Y FACTURA FINAL."  + "\n"
+                                + "10. MOSTRAR CLIENTES ."  + "\n"
+                                + "11. MOSTRAR EMPLEADOS ."  + "\n"
+                                + "12. ENTREGAR VEHICULO(S) Y FACTURA FINAL."  + "\n"
                                 + "0. SALIR."  + "\n");
                                 option= int.Parse(Console.ReadLine());
 
             }catch(Exception e){
-                Console.WriteLine("Digite un valor valido, Vuelva a intentarlo");
+                Console.WriteLine("Digite un valor valido, Vuelva a intentarlo. "+ e.Message);
             }
             return option;
 
