393499d88186aef5224ad39fe2a06775f8d2bb01
Giovanny Moreno <giovannysocas@gmail.com>
2023-08-06 20:58:59 -0500

    feat: Create new cliente menu

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
-﻿
+﻿namespace Taller_Mecanico.Class;
+
 
 internal class Program
 {
     private static void Main(string[] args)
-    {
-        
+    {   
+        Workshop workshop = new Workshop(1,"Honda motors", "Mrs.Hundido");
+        int mainOption = 1;
+        do{
+            MainMenu mainMenu = new MainMenu();
+            mainOption = mainMenu.Menu();
+
+            switch(mainOption){
+                case 1:
+                    Customer customer = new Customer();
+                    Customer newCustomer = customer.Add();
+                    Console.WriteLine(newCustomer.Name);
+                    workshop.Customers.Add(newCustomer);
+                    workshop.ShowCustomers();
+
+                break;
+                case 10: 
+                    workshop.ShowCustomers();
+                break;
+                case 0:
+                 Console.WriteLine("Apagando...");
+                break;
+            }
 
 
+        }while(mainOption != 0);
         
+        
+       
     }
 }
