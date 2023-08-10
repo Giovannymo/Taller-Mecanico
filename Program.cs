676638a53e59c82cdd46fc5becbd1c22bf828c31
Giovanny Moreno <giovannysocas@gmail.com>
2023-08-10 12:51:16 -0500

    case 10 no works yet

--- Program.cs
+++ Program.cs
@@ -11,10 +11,14 @@ internal class Program
             MainMenu mainMenu = new MainMenu();
             int mainOption = mainMenu.Menu();
 
+            Customer customer = new Customer();
+            Employee employee = new Employee();
+            Approved approved = new Approved();
+
+
             switch(mainOption){
                 //Create and Add a new Customer
                 case 1:
-                    Customer customer = new Customer();
                     Customer newCustomer = customer.Add();
                     workshop.Customers.Add(newCustomer);
                     workshop.ShowCustomers();
@@ -22,15 +26,13 @@ internal class Program
                     break;
                 //Deleted and Update List Customers of Workshop
                 case 2:
-                    Customer customerRemove = new Customer(); 
-                    List<Person> newListCostumers = customerRemove.Remove(workshop.Customers);
+                    List<Person> newListCostumers = customer.Remove(workshop.Customers);
                     workshop.Customers = newListCostumers;
                     workshop.ShowCustomers();
 
                     break;
                 //Create and Add a new Employee
                 case 3:
-                    Employee employee = new Employee();
                     Employee newEmployee = employee.Add();
                     workshop.Employees.Add(newEmployee);
                     workshop.ShowEmployees();
@@ -38,8 +40,7 @@ internal class Program
                     break;
                 //Deleted and Update List Employees of Workshop
                 case 4:
-                    Employee employeeRemove = new Employee();
-                    List<Person> newListEmployees = employeeRemove.Remove(workshop.Employees);
+                    List<Person> newListEmployees = employee.Remove(workshop.Employees);
                     workshop.Employees = newListEmployees;
                     workshop.ShowEmployees();
 
@@ -82,13 +83,28 @@ internal class Program
                     workshop.Spares.Add(spare.AddSpare());
                     Console.WriteLine("El repuesto se agregó con éxito. \n");
                     break;
-                case 10: 
+                //Create a new order
+                case 8:
+                    Order order = new Order();
+                    order.GenerateOrder(workshop.Customers, workshop.Employees);
+                    break;
+                //Add spare to Order Approved
+                case 9:
+                    approved.AddSpareApproved(workshop.Spares);
+                    break;
+                //Client aproved Spared
+                case 10:
+                    Console.WriteLine("Ingrese el id del cliente: ");
+                    long idCustomer = long.Parse(Console.ReadLine());
+                    Customer foundCustomer = workshop.Customers.Find( customer => customer.Id == idCustomer );
+
+                case 11: 
                     workshop.ShowCustomers();
                     break;
-                case 11:
+                case 12:
                     workshop.ShowEmployees();
                     break;    
-                case 12:
+                case 13:
                     workshop.ShowSpares();
                     break;
                 case 0:
